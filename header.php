d8c08dafb3c9cb6f432abb9b7d5755d35d9840fc
simran agarwal <167516278+simranagarwal14@users.noreply.github.com>
2025-11-14 13:50:04 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,14 +1,27 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Get the current page filename
+$current_page = basename($_SERVER['PHP_SELF']);
+?>
+
 <header>
     <h1>BCA Student Portal</h1>
     <nav>
         <a href="about.php">About</a> |
-        <?php if(isset($_SESSION['user'])): ?>
-            <a href="dashboard.php">Dashboard</a> |
-            <a href="logout.php">Logout</a>
-        <?php else: ?>
-            <a href="index.php">Login</a> |
-            <a href="register.php">Register</a>
-        <?php endif; ?>
+        <a href="dashboard.php">Dashboard</a> |
+
+        <?php
+        // Show Register on login page, Login on register page
+        if($current_page == 'index.php') {
+            echo '<a href="register.php">Register</a>';
+        } elseif($current_page == 'register.php') {
+            echo '<a href="index.php">Login</a>';
+        }
+        ?>
     </nav>
 </header>
 <hr>
+ 
